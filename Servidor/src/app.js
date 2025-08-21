12efdf6e71c0441737caaf1269f134a54595700c
EmmanuelDelgado24 <delgaemma9912@gmail.com>
2025-08-21 12:18:05 -0600

    agregue cambios app.js

--- Servidor/src/app.js
+++ Servidor/src/app.js
@@ -10,7 +10,7 @@ const app = express();// Inicializa la aplicación Express
 // Middleware de configuración global
 // Habilita CORS para permitir peticiones desde diferentes orígenes.
 app.use(cors({
-    origin: 'https://innovatube-gfyl.onrender.com',
+    origin: 'https://innovatube-u371.onrender.com',
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type'
 }))
